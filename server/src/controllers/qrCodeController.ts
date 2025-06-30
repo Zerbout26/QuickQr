5984608f0ebcf2993bf3f45ba8a31a4d90764899
Riadh26 <mohamedriadhazerbout@MacBook-Pro-de-mohamedriadhazerbout.local>
2025-06-30 23:47:16 +0100

    update

--- server/src/controllers/qrCodeController.ts
+++ server/src/controllers/qrCodeController.ts
@@ -1138,7 +1138,7 @@ export const incrementScanCount = async (req: Request, res: Response) => {
 
 export const getSitemap = async (req: Request, res: Response) => {
   try {
-    const baseUrl = 'https://www.qrcreator.xyz'; // Always use the public domain
+    const baseUrl = 'https://qrcreator.xyz'; // Always use the public domain
     const qrRepo = AppDataSource.getRepository(QRCode);
     const qrCodes = await qrRepo.find({ select: ['id'] });
     const urls = qrCodes.map((qr: { id: string }) =>
